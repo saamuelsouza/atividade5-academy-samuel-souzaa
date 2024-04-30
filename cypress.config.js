e7d5c22ed3064ee576f4f9e170d4620c344304db
Samuel Souza <saamuelfelipe@gmail.com>
2024-04-30 15:54:16 -0300

    Configurados os campos de url em cypress.config .

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    env: {
+      baseUrlBackEnd: "https://rarocrud-80bf38b38f1f.herokuapp.com",
+      baseUrlFrontend: "https://rarocrud-frontend-88984f6e4454.herokuapp.com",
+    },
   },
 });
